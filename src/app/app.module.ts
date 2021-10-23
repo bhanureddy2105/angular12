bbc99ce5f107baf5de407ad7d1b86169defd4012
koumudi-panguluri <59995682+purnimasaikoumudi@users.noreply.github.com>
2021-10-23 23:18:26 +0530

    registration basic form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationComponent } from './registration/registration.component';
+import { AppMaterialModule } from './app-ui.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+  isLoading: boolean = false;
+  registrationFormGroup = new FormGroup({
+    gender: new FormControl('', [Validators.required]),
+    firstName: new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z \-']+")]),
+    lastName: new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z \-']+")]),
+    dob: new FormControl('', [Validators.required]),
+    maritalStatus: new FormControl('', [Validators.required]),
+    pincode: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+  maritalStatus = ["married", "unmarried"];
+  startDate = new Date(1990, 0, 1);
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
